f3847cde9007feb27bf15553e2b381313d70ec64
akhileshawachat <akhileshawachat65@gmail.com>
2024-07-06 13:10:10 +0530

    test 4

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     supabaseUrl: "https://ljmdavfzgrwayxnradil.supabase.co",
     supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqbWRhdmZ6Z3J3YXl4bnJhZGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3MzAwODIsImV4cCI6MjAzNDMwNjA4Mn0.p-on2DMR5_-hi0i3d_P-NcU0xvt3fk-GIkbDHqe7ICE",
     
